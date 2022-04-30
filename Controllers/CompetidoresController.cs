7c123f549114ff88b1936d4b3c77621b0e722b66
Matheus Santon Galter <matheussantong@gmail.com>
2022-04-29 21:42:25 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CompetidoresController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QyonAdventureWorks.Models;
+using Newtonsoft.Json;
+
+namespace QyonAdventureWorks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompetidoresController : ControllerBase
+    {
+        public static List<Competidores> lista = new List<Competidores>();
+
+        [HttpGet]
+        public List<Competidores> GetCompetidores()
+        {
+            return lista;
+        }
+
+        [HttpPost]
+        public string PostCompetidores(Competidores competidores)
+        {
+            lista.Add(competidores);
+            return "Estado cadastrado com sucesso";
+        }
+
+        [HttpPut]
+        public string PutCompetidores(Competidores competidores)
+        {
+            Competidores competidorAux = lista.Where(x => x.Id == competidores.Id).FirstOrDefault();
+            competidorAux.Nome = competidores.Nome;
+            return "Estado alterado com sucesso";
+        }
+
+
+        [HttpDelete]
+        public string DeleteCompetidores(Competidores competidores)
+        {
+            Competidores competidorAux = lista.Where(x => x.Id == competidores.Id).FirstOrDefault();
+            lista.Remove(competidorAux);
+            return "Estado excluído com sucesso";
+        }
+
+    }
+}
---
+++ Models/Competidores.cs
@@ -0,0 +1,12 @@
+﻿namespace QyonAdventureWorks.Models
+{
+    public class Competidores
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string TemperaturaMediaCorpo { get; set; }
+        public string Peso { get; set; }
+        public string Altura { get; set; }
+    }
+}
