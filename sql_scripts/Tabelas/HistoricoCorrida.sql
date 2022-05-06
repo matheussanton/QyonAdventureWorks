7ce21eb68e5378bc66e44081b96b1e9520431639
Matheus Santon Galter <matheussantong@gmail.com>
2022-05-06 17:32:12 -0300

    Updated Sql_Scripts/HistoricoCorrida.sql (with the correct table creation script)

--- sql_scripts/Tabelas/HistoricoCorrida.sql
+++ sql_scripts/Tabelas/HistoricoCorrida.sql
@@ -1,10 +1,17 @@
-CREATE TABLE IF NOT EXISTS public.competidores
+CREATE TABLE IF NOT EXISTS public.historicocorrida
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    nome character varying(150) COLLATE pg_catalog."default" NOT NULL,
-    sexo character(1) COLLATE pg_catalog."default" NOT NULL,
-    temperaturamediacorpo numeric NOT NULL,
-    peso numeric NOT NULL,
-    altura numeric NOT NULL,
-    CONSTRAINT competidores_pkey PRIMARY KEY (id)
+    competidorid integer NOT NULL,
+    pistacorridaid integer NOT NULL,
+    datacorrida date NOT NULL,
+    tempogasto numeric NOT NULL,
+    CONSTRAINT historicocorrida_pkey PRIMARY KEY (id),
+    CONSTRAINT historicocorrida_competidorid_fkey FOREIGN KEY (competidorid)
+        REFERENCES public.competidores (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT historicocorrida_pistacorridaid_fkey FOREIGN KEY (pistacorridaid)
+        REFERENCES public.pistacorrida (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
